85c9163188214c7e8742bf9bbe121aeb8193304f
devdiagon <fredericktimo@gmail.com>
2026-06-12 22:19:18 -0500

    feat(isp): agregar código de ejecución de ejemplo en main

---
+++ src/04-isp/main.ts
@@ -0,0 +1,14 @@
+import { Toucan, Hummingbird, Ostrich } from './bird-catalog';
+
+const toucan      = new Toucan();
+const hummingbird = new Hummingbird();
+const ostrich     = new Ostrich();
+
+toucan.eat();
+toucan.fly();
+
+hummingbird.eat();
+hummingbird.fly();
+
+ostrich.eat();
+ostrich.swim();
